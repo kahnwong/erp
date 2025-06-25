0973dab29151f8142a8f539f4bd343ea3e00a049
Karn Wong <karn@karnwong.me>
2025-06-25 17:21:42 +0700

    feat(add): add auto completion

---
+++ README.md
@@ -0,0 +1,3 @@
+# erp-cli
+
+Because I need a way to list all my perishables and consumables. And I'm a CLI junkie.
--- cmd/add.go
+++ cmd/add.go
@@ -6,9 +6,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func CategoryGet(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	var autocomplete []string
+
+	if len(args) == 0 {
+		autocomplete = core.AppConfig.Categories
+	}
+
+	return autocomplete, cobra.ShellCompDirectiveNoFileComp
+}
+
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add item",
+	Use:               "add",
+	Short:             "Add item",
+	ValidArgsFunction: CategoryGet,
 	Run: func(cmd *cobra.Command, args []string) {
 		core.Add(args)
 	},
